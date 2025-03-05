613afdd60198a8ec0b1f9ddc8040e8c216dbbb1e
Alexandr Novoselov <a.novoselov@informer.com>
2025-03-05 13:21:02 -0300

    Added themes, small updates in the init script.

--- wp_init.sh
+++ wp_init.sh
@@ -12,7 +12,19 @@ wp core install \
 
 wp option update --allow-root siteurl ${WP_URL}:${APP_PORT}
 
-#docker-compose exec -T wp-cli wp scaffold _s \
-#  sample-theme \
-#  --theme_name="Sample Theme" \
-#  --author="John Doe"
+# устанавливаем права
+chmod -R g+w wp-content
+chown -R www-data:www-data wp-content/themes/
+
+# удаляем лишние темы
+su -c "wp theme delete twentytwentyfive twentytwentyfour twentytwentythree" wpuser
+
+# активируем нашу тему
+su -c "wp theme activate test_work_35" wpuser
+
+# эти шаги не нужны, т.к. темы берутся из смонтированного volume
+# устанавливаем тему storefront
+# su -c "wp theme install --activate https://downloads.wordpress.org/theme/storefront.4.6.1.zip" wpuser
+# создаем дочернюю тему
+# su -c "wp scaffold child-theme --parent_theme=storefront --activate test_work_35" wpuser
+
